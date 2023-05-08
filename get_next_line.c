e54ae871a6e4ebd6fddef277842d011d09e16b3a
Axel Caceres <acaceres@c1r12s6.42madrid.com>
2023-05-08 09:05:49 +0200

    trying

--- get_next_line.c
+++ get_next_line.c
@@ -11,11 +11,9 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <string.h>
 
 int		ft_set_lst(int fd, t_list **lst);
-char	*ft_set_line(t_list **lst);
+char    *ft_set_line(t_list **lst);
 
 char	*get_next_line(int fd)
 {
@@ -30,10 +28,10 @@ char	*get_next_line(int fd)
 	set_lst = ft_set_lst(fd, &lst);
 	if (!set_lst)
 		return (ft_lstclear(&lst, free), NULL);
-	line = ft_set_line(&lst);
+    line = ft_set_line(&lst);
 	if (!line)
 		return (ft_lstclear(&lst, free), NULL);
-ft_lstprint(lst);
+    // ft_lstprint(lst);
 	return (line);
 }
 
@@ -69,67 +67,32 @@ int	ft_set_lst(int fd, t_list **lst)
 	return (1);
 }
 
-char	*ft_set_line(t_list **lst)
+char    *ft_set_line(t_list **lst)
 {
-	t_list	*new_node;
-	t_list	*last_node;
-	t_list	*current;
-	char	*line;
-	int		i;
-	int		j;
-  int   h;
+  char  *line;
+  t_list    *current;
+  int       i;
+  int       j;
 
-	if (!*lst)
-		return (0);
-	line = 0;
-	i = 0;
-	j = 0;
-  h = 0;
-	new_node = 0;
-	current = *lst;
-	last_node = *lst;
-	while (last_node->next)
-	{
-		last_node = last_node->next;
-		j++;
-	}
-	while (last_node->content[i] && last_node->content[i] != '\n')
-		i++;
-	line = (char *)malloc(((BUFFER_SIZE * j) + i) * sizeof(char));
-	if (!line)
-		return (free(line), NULL);
-	j = i + 2;
-	while (last_node->content[i])
-		i++;
-	new_node = (t_list *)malloc(sizeof(t_list));
-	if (!new_node)
-		return (free(line), NULL);
-	new_node->content = (char *)malloc((i - 1 - j + 1) * sizeof(char));
-	if (!new_node->content)
-		return (free(new_node->content), free(new_node), free(line), NULL);
-	new_node->next = NULL;
-  while (j < i)
-    new_node->content[h++] = last_node->content[j++];
-  new_node->content[h] = 0;
-	i = 0;
-	j = 0;
-	while (current)
-	{
-		while (current->content[i])
-		{
-			if (current->content[i] == '\n')
-			{
-				line[j++] = current->content[i++];
-				line[j] = '\0';
-        ft_lstclear(lst, free);
-        ft_lstadd_back(lst, new_node);
-				return (line);
-			}
-			line[j++] = current->content[i++];
-		}
-		i = 0;
-		current = current->next;
-	}
-	line[j] = 0;
-	return (line);
+  line = 0;
+  i = 0;
+  j = 0;
+  current = *lst;
+  while (current)
+  {
+    while (current->content[i])
+    {
+      if (current->content[i])
+      {
+        line[j++] = current->content[i++];
+        line[j] = 0;
+        return (line);
+      }
+      line[j++] = current->content[i++];
+    }
+    i = 0;
+    current = current->next;
+  }
+  line[j] = 0;
+  return (line);
 }
