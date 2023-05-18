8d87e15c5e327df02e0153f4c83e074af1e14998
Axel Caceres <acaceres@c1r7s4.42madrid.com>
2023-05-18 17:41:27 +0200

    making bonus

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: acaceres <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/02 07:46:51 by acaceres          #+#    #+#             */
-/*   Updated: 2023/05/08 15:16:17 by acaceres         ###   ########.fr       */
+/*   Created: 2023/05/11 12:59:41 by acaceres          #+#    #+#             */
+/*   Updated: 2023/05/11 13:02:13 by acaceres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,16 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-void	ft_lstprint(t_list *node)
-{
-	if (!node)
-		write(1, "Node doesn't exist", 18);
-	while (node)
-	{
-		while (*(char *)node->content)
-			write(1, (char *)node->content++, 1);
-		node = node->next;
-	}
-}
-
 int	main(void)
 {
 	int		fd;
-    char	*line;
+	char	*line;
 
 	fd = open("the_bible.txt", O_RDONLY);
 	while ((line = get_next_line(fd)))
 	{
-		printf("Line = %s", line);
+		printf("Line: %s", line);
 		free(line);
 	}
-	close(fd);
 	return (0);
 }
